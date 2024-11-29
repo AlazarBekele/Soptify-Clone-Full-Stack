8a73e71733162ab6471215007c6a9acf2c3640e3
AlazarBekele <alazarthe70@gmail.com>
2024-11-29 21:05:27 +0300

    Debug the static

--- Music/urls.py
+++ Music/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, login_Page
 
 
 urlpatterns = [
-    path ('', index, name="index")
+    path ('', index, name="index"),
+    path ('login/', login_Page, name="login_Page")
 ]
