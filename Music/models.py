922648b3842deb928f25c5e203be5b1f49968335
AlazarBekele <alazarthe70@gmail.com>
2024-11-30 16:09:39 +0300

    Create login in Backend form

--- Music/models.py
+++ Music/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Catagory (models.Model):
+
+  Title = models.CharField (max_length=30)
+
+  def __str__(self) -> str:
+    return self.Title
+  
+
+class Login_Data (models.Model):
+
+  first_name = models.CharField (max_length=30)
+  last_name = models.CharField (max_length=30)
+
+  def __str__(self) -> str:
+    return self.first_name + self.last_name
+  
+
+class Post_Music (models.Model):
+
+  artist_Image = models.ImageField (upload_to='Artist_Image/')
+  Music_Title = models.CharField (max_length=20)
+  artist = models.CharField (max_length=30)
+  release_date = models.DateField (null=False, blank=False, auto_created=True, auto_now_add=True)
+
+  def __str__(self) -> str:
+    return self.artist
