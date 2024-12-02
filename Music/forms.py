7a26a8934afb9f6a5215456f94087bec9225dc46
AlazarBekele <alazarthe70@gmail.com>
2024-12-02 20:34:51 +0300

    Start Main Body

--- Music/forms.py
+++ Music/forms.py
@@ -7,32 +7,32 @@ from django.contrib.auth.models import User
 class LogInput (UserCreationForm):
       
       first_name = forms.CharField (max_length=30, widget = forms.TextInput(attrs={
-         'class' : 'form-control',
+         'class' : 'form-control parkinsans',
          'placeholder' : 'First Name',
          'autocomplate' : 'off'
       }))
       last_name = forms.CharField (max_length=30, widget = forms.TextInput(attrs={
-         'class' : 'form-control',
+         'class' : 'form-control parkinsans',
          'placeholder' : 'last Name',
          'autocomplate' : 'off'
       }))
       username = forms.CharField (widget = forms.TextInput(attrs={
-         'class' : 'form-control',
+         'class' : 'form-control parkinsans',
          'placeholder' : 'Enter username',
          'autocomplate' : 'off'
       }))
       Email_Part= forms.EmailField (widget = forms.EmailInput(attrs={
-         'class' : 'form-control',
+         'class' : 'form-control parkinsans',
          'placeholder' : 'Enter Email',
          'autocomplate' : 'off'
       }))
       password1 = forms.CharField (max_length=40, label='password', widget = forms.PasswordInput(attrs={
-         'class' : 'form-control',
+         'class' : 'form-control parkinsans',
          'placeholder' : 'Enter Password1',
          'autocomplete': 'off'
       }))
       password2 = forms.CharField( max_length=40, label='Confirm Password', widget=forms.PasswordInput(attrs={
-          'class' : 'form-control',
+          'class' : 'form-control parkinsans',
           'placeholder' : 'Confirm Password',
           'autocomplete': 'off'
       }))
