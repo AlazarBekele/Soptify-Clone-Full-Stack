fb2d214bfa49bcb8457cbf4696d8bace407705b7
AlazarBekele <alazarthe70@gmail.com>
2024-12-03 23:10:33 +0300

    Successfully Post Work

--- Music/admin.py
+++ Music/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+  Catagory,
+  Post_Music
+)
 
 # Register your models here.
+
+admin.site.register (Catagory)
+admin.site.register (Post_Music)
--- Music/urls.py
+++ Music/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, login_Page, sign_up, post_page
+from .views import index, login_Page, sign_up, post_page, payment
 
 
 urlpatterns = [
     path ('', index, name="index"),
     path ('login/', login_Page, name="login"),
     path ('signup/', sign_up, name="sign_up"),
-    path ('post/', post_page, name='post_page')
+    path ('post/', post_page, name='post_page'),
+    path ('payment/', payment, name="pay")
 ]
